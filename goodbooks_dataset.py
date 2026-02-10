478810e63c0963aec3f2c8c162bd49d1177f3898
Jonathan Chamberlin <jcham17x@gmail.com>
2026-02-09 20:59:50 -0500

    Load CSVs from local files instead of remote URLs for faster iteration

--- goodbooks_dataset.py
+++ goodbooks_dataset.py
@@ -1,22 +1,22 @@
 import numpy as np
 import pandas as pd
 
-books = pd.read_csv('https://raw.githubusercontent.com/zygmuntz/goodbooks-10k/refs/heads/master/books.csv')
+books = pd.read_csv('data/books.csv')
 
 print(books.iloc[0])
 
-ratings = pd.read_csv('https://raw.githubusercontent.com/zygmuntz/goodbooks-10k/refs/heads/master/ratings.csv')
+ratings = pd.read_csv('data/ratings.csv')
 
 print(ratings.iloc[0])
 
-book_tags = pd.read_csv('https://raw.githubusercontent.com/zygmuntz/goodbooks-10k/refs/heads/master/book_tags.csv')
+book_tags = pd.read_csv('data/book_tags.csv')
 
 print(book_tags.iloc[0])
 
-to_read = pd.read_csv('https://raw.githubusercontent.com/zygmuntz/goodbooks-10k/refs/heads/master/to_read.csv')
+to_read = pd.read_csv('data/to_read.csv')
 
 print(to_read.iloc[0])
 
-tags = pd.read_csv('https://raw.githubusercontent.com/zygmuntz/goodbooks-10k/refs/heads/master/tags.csv')
+tags = pd.read_csv('data/tags.csv')
 
 print(tags.iloc[0])
